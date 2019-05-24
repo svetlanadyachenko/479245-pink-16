8de7b711ac569e677bc12982007ba5395e4ae869
Sveta <angelinalana84@gmail.com>
2019-05-24 10:57:45 +0300

    Исправление Б4, Д14

--- source/js/scriptform.js
+++ source/js/scriptform.js
@@ -1,20 +1,15 @@
 var navMain = document.querySelector(".page-header");
-var navToggleClose = document.querySelector(".main-header__toggle-close");
-var navToggleOpen = document.querySelector(".main-header__toggle-open");
+var navToggle = document.querySelector(".main-header__toggle");
 
 navMain.classList.remove("modal-nojs");
 
-navToggleClose.addEventListener("click", function(){
-  if (navMain.classList.contains("modal-opened")) {
-    navMain.classList.remove("modal-opened");
-    navMain.classList.add("modal-closed");
-  }
-});
-
-navToggleOpen.addEventListener("click", function(){
+navToggle.addEventListener("click", function(){
   if (navMain.classList.contains("modal-closed")) {
     navMain.classList.remove("modal-closed");
     navMain.classList.add("modal-opened");
+  } else {
+    navMain.classList.remove("modal-opened");
+    navMain.classList.add("modal-closed");
   }
 });
 
